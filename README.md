1ec76e38c2908a7589244c08c9d52b03f46abf51
Christian Kraemer <Christian.Kraemer@tngtech.com>
2022-03-27 22:27:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Installation
+
+- Install [platformio](https://platformio.org/)
+- Edit platformio.ini and set correct upload port
+- Connect the device via usb
+- To flash the firmware run the following command:
+```bash
+  pio run --target=upload
+```
---
+++ src/boost.ino
@@ -0,0 +1,92 @@
+/**
+ * A Boost basic example to connect a boost hub, set the led color and the name of the hub and
+ * do some basic movements on the boost map grid
+ * 
+ * (c) Copyright 2020 - Cornelius Munz
+ * Released under MIT License
+ * 
+ */
+
+#include "Boost.h"
+
+// create a hub instance
+Boost myMoveHub;
+byte portC = (byte)MoveHubPort::C;
+byte portD = (byte)MoveHubPort::D;
+boolean isInitialized = false;
+
+void setup()
+{
+  Serial.begin(115200);
+  myMoveHub.init(); // initalize the MoveHub instance
+  Serial.println("Setup complete");
+}
+
+// main loop
+void loop()
+{
+  // connect flow. Search for BLE services and try to connect if the uuid of the hub is found
+  if (myMoveHub.isConnecting())
+  {
+    myMoveHub.connectHub();
+    if (myMoveHub.isConnected() && !isInitialized)
+    {
+      Serial.println("Connected to HUB");
+      delay(50);
+      myMoveHub.activatePortDevice((byte)MoveHubPort::TILT, portValueChangeCallback);
+      delay(50);
+      isInitialized = true;
+    }
+    else
+    {
+      Serial.println("Failed to connect to HUB");
+    }
+  }
+
+  // if connected, you can set the name of the hub, the led color and shut it down
+  if (myMoveHub.isConnected())
+  {
+
+    char hubName[] = "myMoveHub";
+    myMoveHub.setHubName(hubName);
+    myMoveHub.setLedColor(GREEN);
+    delay(1000);
+    myMoveHub.setLedColor(RED);
+    delay(1000);
+
+    // lets do some movements on the boost map
+    myMoveHub.moveForward(1);
+    delay(2000);
+    myMoveHub.rotateLeft(90);
+    delay(2000);
+    myMoveHub.moveForward(1);
+    delay(2000);
+    myMoveHub.rotateRight(90);
+    delay(2000);
+    myMoveHub.moveBack(1);
+    delay(2000);
+    myMoveHub.moveArcLeft(90);
+    delay(2000);
+    myMoveHub.moveArcRight(90);
+    delay(2000);
+    myMoveHub.setTachoMotorSpeedForDegrees(portC, 50, 1 * 360 * 2);
+    delay(2000);
+  }
+
+} // End of loop
+
+// callback function to handle updates of sensor values
+void portValueChangeCallback(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData)
+{
+  Lpf2Hub *myHub = (Lpf2Hub *)hub;
+
+  if (deviceType == DeviceType::MOVE_HUB_TILT_SENSOR)
+  {
+    int x = myHub->parseBoostTiltSensorX(pData);
+    int y = myHub->parseBoostTiltSensorY(pData);
+    Serial.print("Tilt X: ");
+    Serial.print(x, DEC);
+    Serial.print(" Y: ");
+    Serial.println(y, DEC);
+  }
+}
